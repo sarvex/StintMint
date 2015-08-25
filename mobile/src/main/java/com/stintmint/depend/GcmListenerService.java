d0f5ddb03d27166917bc192dc0c2affe52c651ae
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-08-25 18:36:49 +0530

    Blank Login & SignUp

--- mobile/src/main/java/com/stintmint/depend/GcmListenerService.java
+++ mobile/src/main/java/com/stintmint/depend/GcmListenerService.java
@@ -1,7 +1,17 @@
 package com.stintmint.depend;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Sarvex Jatasra on 8/25/2015.
  */
-public class GcmListenerService {
+public class GcmListenerService extends Service {
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
 }
