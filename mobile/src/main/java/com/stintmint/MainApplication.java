595cf4ce78b7df4212cd772893b3f399d3fb5896
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-08-25 16:12:47 +0530

    Parse Integration

---
+++ mobile/src/main/java/com/stintmint/MainApplication.java
@@ -0,0 +1,28 @@
+package com.stintmint;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.parse.Parse;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Sarvex Jatasra on 8/25/2015.
+ */
+public class MainApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    Parse.enableLocalDatastore(this);
+    Parse.initialize(this, BuildConfig.PARSE_KEY, BuildConfig.PARSE_TOKEN);
+
+    TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+    Fabric.with(this, new Crashlytics(), new Twitter(authConfig));
+
+  }
+}
--- wear/src/main/java/com/stintmint/MainActivity.java
+++ wear/src/main/java/com/stintmint/MainActivity.java
@@ -1,14 +1,23 @@
 package com.stintmint;
 
+import android.R.color;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
 import android.widget.TextView;
 
+import com.crashlytics.android.Crashlytics;
+import com.stintmint.R.id;
+import com.stintmint.R.layout;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends WearableActivity {
 
@@ -22,43 +31,45 @@ public class MainActivity extends WearableActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setAmbientEnabled();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new Crashlytics(), new Twitter(authConfig));
+        this.setContentView(layout.activity_main);
+        this.setAmbientEnabled();
 
-        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        mClockView = (TextView) findViewById(R.id.clock);
+        this.mContainerView = (BoxInsetLayout) this.findViewById(id.container);
+        this.mTextView = (TextView) this.findViewById(id.text);
+        this.mClockView = (TextView) this.findViewById(id.clock);
     }
 
     @Override
     public void onEnterAmbient(Bundle ambientDetails) {
         super.onEnterAmbient(ambientDetails);
-        updateDisplay();
+        this.updateDisplay();
     }
 
     @Override
     public void onUpdateAmbient() {
         super.onUpdateAmbient();
-        updateDisplay();
+        this.updateDisplay();
     }
 
     @Override
     public void onExitAmbient() {
-        updateDisplay();
+        this.updateDisplay();
         super.onExitAmbient();
     }
 
     private void updateDisplay() {
-        if (isAmbient()) {
-            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
-            mTextView.setTextColor(getResources().getColor(android.R.color.white));
-            mClockView.setVisibility(View.VISIBLE);
+        if (this.isAmbient()) {
+            this.mContainerView.setBackgroundColor(this.getResources().getColor(color.black));
+            this.mTextView.setTextColor(this.getResources().getColor(color.white));
+            this.mClockView.setVisibility(View.VISIBLE);
 
-            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+            this.mClockView.setText(MainActivity.AMBIENT_DATE_FORMAT.format(new Date()));
         } else {
-            mContainerView.setBackground(null);
-            mTextView.setTextColor(getResources().getColor(android.R.color.black));
-            mClockView.setVisibility(View.GONE);
+            this.mContainerView.setBackground(null);
+            this.mTextView.setTextColor(this.getResources().getColor(color.black));
+            this.mClockView.setVisibility(View.GONE);
         }
     }
 }
